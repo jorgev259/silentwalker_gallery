99cb7a5324f1b648045c6c67c3ffe31248239876
jorgev259 <jorgev259@outlook.es>
2022-05-13 14:33:42 -0600

    Fix image cleanup filter

--- cleanBuild.js
+++ cleanBuild.js
@@ -4,7 +4,9 @@ const fs = require('fs-extra')
 main()
 
 async function main () {
-  const files = glob.sync('out/images/drive_gallery/**/*.{jpg,jpeg,png}')
+  const files = glob
+    .sync('out/images/drive_gallery/**/*.{jpg,jpeg,png}')
+    .filter(p => !p.includes('nextImageExportOptimizer'))
   console.log(`Found ${files.length} image files to delete`)
 
   await Promise.all(files.map(p => fs.remove(p)))
