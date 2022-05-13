778d636800d1dbb6a0f80da855ba65926ae437b1
jorgev259 <jorgev259@outlook.es>
2022-05-13 14:09:46 -0600

    .

--- cleanBuild.js
+++ cleanBuild.js
@@ -5,5 +5,8 @@ main()
 
 async function main () {
   const files = glob.sync('out/images/drive_gallery/**/*.{jpg,jpeg,png}')
+  console.log(`Found ${files.length} image files to delete`)
+
   await Promise.all(files.map(p => fs.remove(p)))
+  console.log('Source images deleted from out folder')
 }
