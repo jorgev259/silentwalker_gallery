01638f755685a5c7447842cdfec6888e3a4c9918
jorgev259 <jorgev259@outlook.es>
2022-05-13 19:33:18 -0600

    .

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 const path = require('path')
 
 module.exports = {
-  images: {
-    loader: 'custom',
-    formats: ['image/webp'],
-    nextImageExportOptimizer: {
-      imageFolderPath: 'public/images',
-      exportFolderPath: 'out',
-      quality: 75
-    }
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
   }
