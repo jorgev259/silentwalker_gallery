3ed1ba27ad57d55cbea2f456c14c6fa6d029a43b
jorgev259 <jorgev259@outlook.es>
2022-05-23 00:16:54 -0600

    Remove and skip clan banners

--- createThumbs.js
+++ createThumbs.js
@@ -3,20 +3,19 @@ const glob = require('glob')
 const fs = require('fs-extra')
 const checksum = require('checksum')
 
+const clanFolder = 'wallpapers/Destiny 2/Clan Banners DM or tweet me @silentwalker__ for more info'
 const quality = 30
 const cache = fs.readJSONSync('./thumbCache.json', { throws: false }) || {}
 
 let max
 let current = 0
 
-function skipFile (outFilePath) {
+function skipFile (outFilePath, log = true) {
   current++
-  console.log(`Skipped "${outFilePath}" (${current}/${max})`)
+  if (log) console.log(`Skipped "${outFilePath}" (${current}/${max})`)
 }
 
 async function optimiseFile (filePath) {
-  const check = checksum(filePath)
-
   let parentDir = filePath.split('/')
   const fileName = parentDir.pop().split('.').shift()
   parentDir = parentDir.join('/')
@@ -24,6 +23,10 @@ async function optimiseFile (filePath) {
 
   const outputDir = parentDir.replace('wallpapers/', 'thumbs/')
   const outputPath = `${outputDir}/${fileName}`
+
+  if (filePath.startsWith(clanFolder)) return skipFile(outFilePath, false)
+
+  const check = checksum(filePath)
 
   if (
     cache[filePath] === check ||
