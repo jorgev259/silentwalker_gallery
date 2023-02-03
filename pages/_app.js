783b5dd75bc668642e7310bf595ec50ec0181462
jorgev259 <jorgev259@outlook.es>
2023-02-02 19:46:33 -0600

    Use react-ga4

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
 import Head from 'next/head'
-import Script from 'next/script'
+import ReactGA from 'react-ga4'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.scss'
 
 import Navbar from '../components/Navbar'
+
+ReactGA.initialize('G-1KJXLJMYNF')
 
 export default function App ({ Component, pageProps }) {
   const [sort, setSort] = useState('new')
@@ -27,16 +29,6 @@ export default function App ({ Component, pageProps }) {
         <meta key='desc' property='og:description' content='The biggest collection of Destiny emblem wallpapers. Made by SiLeNtWaLkEr' />
         <meta key='image' property='og:image' content='/images/assets/logo_only.png' />
       </Head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-1KJXLJMYNF" />
-      <Script id="gtag">
-        {`
-           window.dataLayer = window.dataLayer || [];
-           function gtag(){dataLayer.push(arguments);}
-           gtag('js', new Date());
- 
-           gtag('config', 'G-1KJXLJMYNF');
-          `}
-      </Script>
       <Navbar {...props} setSearch={setSearch} />
       <Component {...pageProps} sort={sort} search={search} />
     </>
