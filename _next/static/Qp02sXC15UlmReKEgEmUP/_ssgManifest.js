92bb88e1e5c216a18f3fecd046c4d86f70121347
jorgev259 <jorgev259@users.noreply.github.com>
2024-04-19 18:44:10 +0000

    deploy: a30c394005906af363f3eb735b716cca84c5933a

---
+++ _next/static/Qp02sXC15UlmReKEgEmUP/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fdestiny1\u002F[device]","\u002Fdestiny1\u002F[device]\u002F[urlName]","\u002Fdestiny2\u002F[device]","\u002Fdestiny2\u002F[device]\u002F[category]","\u002Fdestiny2\u002F[device]\u002F[category]\u002F[urlName]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
